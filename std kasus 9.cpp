09bcee477075ba2054a1771a8a07166b6adc419d
Fahrizky Syihabudin I <101689299+Riez03@users.noreply.github.com>
2022-06-15 13:48:16 +0700

    Studi Kasus
    array 2D ke 1D

---
+++ std kasus 9.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+class Array {
+public:
+  void input();
+  void cetak_satu();
+  void sorting();
+  void konversi();
+  void cetak_dua();
+
+private:
+  int array[20][20], array2[50];
+  int n, z, m, k, temp;
+};
+void Array::input() {
+  cout << "Masukan Jumlah Buku :";
+  cin >> z;
+  for (int i = 0; i < z; i++) {
+    cout << "Nomor buku ke [" << i << "] : ";
+    cin >> array2[i];
+  }
+}
+void Array::cetak_satu() {
+  cout << "Bentuk Array 1D :" << endl;
+  for (int k = 0; k < z; k++) {
+    cout << array2[k] << "  ";
+  }
+}
+void Array::sorting() {
+  for (int i = 0; i < z; i++) {
+    for (int c = 0; c < z - 1; c++) {
+      if (array2[c] > array2[c + 1]) {
+        temp = array2[c];
+        array2[c] = array2[c + 1];
+        array2[c + 1] = temp;
+      }
+    }
+  }
+}
+void Array::konversi() {
+  cout << " \nUbah ke array 2D :\n";
+  cout << " Input jumlah baris : ";
+  cin >> n;
+  cout << " Input jumlah kolom : ";
+  cin >> m;
+  k = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      array[i][j] = array2[k];
+      k++;
+    }
+  }
+}
+void Array::cetak_dua() {
+  cout << "\n Bentuk array 2D :" << endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << array[i][j] << "\t";
+    }
+    cout << endl;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << "Rak ke " << i + 1 << ", kolom ke " << j + 1 << ", Buku nomor "
+           << array[i][j] << endl;
+    }
+  }
+}
+
+int main() {
+  Array run;
+  run.input();
+  run.cetak_satu();
+  run.sorting();
+  run.konversi();
+  run.cetak_dua();
+  return 0;
+}
